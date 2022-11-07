c6203ae75e021a72603356bbb31d44d92517d147
MUNJU <mvv0030@gmail.com>
2022-11-07 13:08:55 +0900

    id 값을 받아 view에 넘겨줌

---
+++ bbs/src/main/java/com/mustache/bbs/controller/MustacheController.java
@@ -0,0 +1,23 @@
+package com.mustache.bbs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class MustacheController {
+
+    @GetMapping(value = "/hi")
+    public String mustacheCon(Model model) {
+        model.addAttribute("username", "ju");
+        return "greetings";
+    }
+
+    @GetMapping(value = "/hi/{id}")
+    public String mustacheCon(Model model, @PathVariable String id) {
+        model.addAttribute("username", "ju");
+        model.addAttribute("id", id);
+        return "greetings";
+    }
+}
